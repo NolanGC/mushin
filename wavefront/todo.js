fb27ef0ec77af899797a82a805b1caa2d9b54c46
Nolan Clement <nolangclement@gmail.com>
2024-11-21 15:16:27 -0500

    philosophy

---
+++ wavefront/todo.js
@@ -0,0 +1,17 @@
+/*
+Mushin - a new kind of text editor
+nomind - a new kind of text editor
+
+
+- figure out how to make the processing block unresponsive, like you cant enter it with a cursor or edit anything
+- figure out how to do buffered replacement and processing
+- make the shimmer loading animation PERFECT and beautiful
+- lots of edge cases to handle
+
+philosophy
+- it should be fast and simple and lightweight and beautiful
+- it should be free (if you want to provide your own model)
+- there should be a finetuned model that you can use with pro
+- actually maybe just open source it? but the hosted verison is paid 
+- self hosting entails downloading it, building the executable, and using your own model
+*/
